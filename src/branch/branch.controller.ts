60da32c99e538d145efd6b632ce7450f10540add
Emperor chris <emperorchris36@gmail.com>
2026-03-26 12:06:55 +0100

    Made branch public

--- src/branch/branch.controller.ts
+++ src/branch/branch.controller.ts
@@ -8,18 +8,19 @@ import { User } from 'src/user/entities/user.entity';
 import { UserRole } from 'generated/prisma/enums';
 import { IsPublic } from 'decorator/isPublic.decorator';
 
+@IsPublic()
 @Controller('branches')
 export class BranchController {
   constructor(private readonly branchService: BranchService) { }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(UserRole.ADMIN, UserRole.DIRECTOR, UserRole.SUPERVISOR)
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(UserRole.ADMIN, UserRole.DIRECTOR, UserRole.SUPERVISOR)
   @Post()
   create(@Body() createBranchDto: CreateBranchDto) {
     return this.branchService.create(createBranchDto);
   }
 
-  @IsPublic()
+  // @IsPublic()
   @Get()
   findAll(@Query('page') page?: string) {
     return this.branchService.findAll(Number(page) || 1);
@@ -30,15 +31,15 @@ export class BranchController {
     return this.branchService.findOne(id);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(UserRole.ADMIN, UserRole.DIRECTOR, UserRole.SUPERVISOR)
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(UserRole.ADMIN, UserRole.DIRECTOR, UserRole.SUPERVISOR)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBranchDto: UpdateBranchDto) {
     return this.branchService.update(id, updateBranchDto);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Roles(UserRole.ADMIN, UserRole.DIRECTOR, UserRole.SUPERVISOR)
+  // @UseGuards(JwtAuthGuard)
+  // @Roles(UserRole.ADMIN, UserRole.DIRECTOR, UserRole.SUPERVISOR)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.branchService.remove(id);
