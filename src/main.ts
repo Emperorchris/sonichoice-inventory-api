84bc1c1d4aa5db8ec9206b910e800a89810a8754
Emperor chris <emperorchris36@gmail.com>
2026-03-28 07:04:01 +0100

    fix: server error

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,8 @@ async function bootstrap() {
 	}));
 	
 	app.useGlobalGuards(jwtAuthGuard, rolesGuard);
-	await app.listen(process.env.PORT ?? 3000);
-	console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+	const port = process.env.PORT ?? 3000;
+	await app.listen(port, '0.0.0.0');
+	console.log(`Application is running on port ${port}`);
 }
 bootstrap();
